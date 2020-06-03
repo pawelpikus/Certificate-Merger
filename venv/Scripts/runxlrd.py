c63809f7b867dfb0c65f026d785fbbd6f3d129e8
godspeed7 <pawelpikus7@gmail.com>
2020-06-03 17:32:23 +0200

    cert_maker.py Done

--- venv/Scripts/runxlrd.py
+++ venv/Scripts/runxlrd.py
@@ -1,4 +1,4 @@
-#!c:\users\pawel\pycharmprojects\certificate_maker\venv\scripts\python.exe
+#!c:\users\godspeed7\pycharmprojects\certificate_maker\venv\scripts\python.exe
 # Copyright (c) 2005-2012 Stephen John Machin, Lingfo Pty Ltd
 # This script is part of the xlrd package, which is released under a
 # BSD-style licence.
