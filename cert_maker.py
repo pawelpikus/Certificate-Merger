54e12db6e37b0655e6993f31a4af0b94fb815d15
Paweł Pikus <pawelpikus7@gmail.com>
2020-06-06 18:04:12 +0200

    certificate_maker.py argv1=template argv2=data_sheet

--- cert_maker.py
+++ cert_maker.py
@@ -2,19 +2,33 @@ from mailmerge import MailMerge
 import pandas as pd
 import sys
 
-template_filename = sys.argv[1]
-data_filename = sys.argv[2]
+try:
+    if sys.argv[1].endswith(('.doc', '.docx')):
+        template_filename = sys.argv[1]
+    else:
+        print('Template must be in a .doc or .docx format.')
+        sys.exit(0)
+    if sys.argv[2].endswith(('.xls', '.xlsx')):
+        data_filename = sys.argv[2]
+    else:
+        print('Data must be in a .xls or .xlsx format.')
+        sys.exit(0)
 
-data = pd.read_excel(data_filename)
-#  change all df values to str, needed by merge_templates()
-data = data.applymap(str)
-data_dict = data.to_dict(orient='index')
+    data = pd.read_excel(data_filename)
+    #  change all df values to str, needed by merge_templates()
+    data = data.applymap(str)
+    data_dict = data.to_dict(orient='index')
 
-students = []
-for key, value in data_dict.items():
-    students.append(data_dict[key])
+    students = []
+    for key in data_dict.keys():
+        students.append(data_dict[key])
 
-with MailMerge(template_filename) as document:
-    document.merge_templates(students, separator='page_break')
-    document.write('certificates.docx')
-print('Done.')
+    with MailMerge(template_filename) as document:
+        document.merge_templates(students, separator='page_break')
+        document.write('certificates.docx')
+    print('Done.')
+except IndexError:
+    print("Type: cert_maker.py [arg1=template] [arg2=data]")
+except FileNotFoundError as err:
+    print(err)
+
